1d96fd24c297fb906dd3ead1c7abc50e2bf10254
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-12-27 14:17:57 +0200

    main

--- section1/main.go
+++ section1/main.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func printStrings() {
+	m1 := "My name: " 
+	m2 := "Richard"
+	fmt.Println(strings.Contains(m1,m2)); // checks if contains the same values
+	fmt.Println(m1+m2);
+	fmt.Println("Hello World!");
+}
+
 
 func main() {
 var m1 int32;
@@ -9,6 +21,7 @@ m3 := 4;
 
 	m1 = 2;
 	m2 = 3;
+	
 	fmt.Println(m1); 
 	fmt.Println(m2);
 	fmt.Println(m3);
@@ -18,4 +31,6 @@ m3 := 4;
 	// fmt.Println(m3+m2); // has to be casted
 	fmt.Println(int32(m3)+m1);
 	fmt.Println(int64(m3)+m2);
+
+	printStrings();
 }
